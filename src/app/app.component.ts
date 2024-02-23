0f54de4ab1ddcb22427908d406b129896af4c018
AdriAguilar <adritrivi41@gmail.com>
2024-02-23 18:50:03 +0100

    Added dark/light mode button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isDarkMode: boolean = false;
+  presentTheme$ = new BehaviorSubject<string>('light');
   phrases: string[] = ["FRIEND", "DEVELOPER", "DESIGNER", "COFFEE DRINKER", "IT RECRUITER", "HR"];
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.presentTheme$.next(savedTheme);
+    }
+  }
+
+  changeTheme(): void {
+    this.presentTheme$.value === 'light'
+      ? this.presentTheme$.next('dark')
+      : this.presentTheme$.next('light');
+    localStorage.setItem('theme', this.presentTheme$.value);
+    this.isDarkMode = !this.isDarkMode;
+  }
 }
