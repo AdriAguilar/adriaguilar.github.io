2880d5f82c11e44f9e35053797d6000333a471d7
Adrian <adritrivi41@gmail.com>
2025-07-12 12:07:08 +0200

    Fixed footer mail href and real time date on header

---
+++ src/components/reactive/CosmeticText.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let text = "";
+  </script>
+  
+  <span class="text-edge block px-2 text-xl font-semibold tracking-[0.3em]">
+    {text}
+  </span>
+  
---
+++ src/components/reactive/CurrentDate.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import CosmeticText from './CosmeticText.svelte';
+
+  let date = new Date().toLocaleDateString("es-ES", {
+    year: "numeric", month: "short", day: "numeric"
+  }).toUpperCase();
+</script>
+
+<CosmeticText text={date} />
