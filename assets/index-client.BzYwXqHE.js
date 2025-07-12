823ac86ef964c70fae4a2eaeb65f19aee9baf221
Adrian <adritrivi41@gmail.com>
2025-07-12 12:07:46 +0200

    Updates

---
+++ assets/index-client.BzYwXqHE.js
@@ -0,0 +1,1 @@
+import{au as t,z as o,E as c,x as l}from"./render.8pdN5EEe.js";function u(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function r(e){t===null&&u(),o&&t.l!==null?a(t).m.push(e):c(()=>{const n=l(e);if(typeof n=="function")return n})}function a(e){var n=e.l;return n.u??={a:[],b:[],m:[]}}export{r as o};
